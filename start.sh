a2eae74b58f886ab4e722425a5ae928b0575ec37
heelaachakzai <heelaachakzai2424@gmail.com>
2026-06-30 16:40:21 +0500

    Create admin user on startup

--- start.sh
+++ start.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 superset db upgrade
+
+superset fab create-admin \
+  --username Heela \
+  --firstname Heela \
+  --lastname Achakzai \
+  --email heelaachakzai2424@mail.com \
+  --password 2424 || true
+
 superset init
 
 exec superset run -h 0.0.0.0 -p 8088
